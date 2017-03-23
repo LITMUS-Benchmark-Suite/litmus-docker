7c3f7b3f2dd94b551fd41df5a11528b13b6d2d9f
yashwantkeswani <yashwant.keswani@gmail.com>
2017-03-23 15:02:06 +0530

    Add bash script to load data

---
+++ openlink/virtuoso_load.sh
@@ -0,0 +1,14 @@
+
+
+Virtuoso_isql=$1;
+Result_file=$2;
+for i in $(seq 1 1 $3)
+do
+    sh -c "sync ; echo 3 > /proc/sys/vm/drop_caches";
+	echo "Run Number:$i"
+	#echo "Run Number:$i" >> $4
+    $Virtuoso_isql 1111 dba dba prepare.sql> /dev/null 2>> /dev/null;
+    /usr/bin/time -a -o $Result_file -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba load.sql> /dev/null 2>> /dev/null;
+    $Virtuoso_isql 1111 dba dba clear.sql> /dev/null 2>> /dev/null;
+done
+
