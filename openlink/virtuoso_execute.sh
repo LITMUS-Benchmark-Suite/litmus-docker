697bd92def177aae38ea0195e583e234ae4ca0e4
yashwantkeswani <yashwant.keswani@gmail.com>
2017-03-26 03:32:41 +0530

    Update Virtuoso Scripts

--- openlink/virtuoso_execute.sh
+++ openlink/virtuoso_execute.sh
@@ -1,17 +1,16 @@
 Virtuoso_isql=$1;
-Result_file=$2;
+RESULT_FILE=$2;
 Query_location=$3;
 
 $Virtuoso_isql 1111 dba dba clear.sql> /dev/null 2>> /dev/null;
 $Virtuoso_isql 1111 dba dba prepare.sql> /dev/null 2>> /dev/null;
 $Virtuoso_isql 1111 dba dba load.sql> /dev/null 2>> /dev/null;
 
-
 for i in `ls $Query_location/*.sparql`; do
     export BASEN=`basename $i .sparql`;
 
     echo "***********" >> $RESULT_FILE;
-    echo $BASEN >> $RESULT_file;    
+    echo $BASEN >> $RESULT_FILE;    
     #echo "------- FLUSHING CACHE -------" >> $RESULT_file;
     echo $BASEN;
     for k in `seq $4`; do
@@ -19,6 +18,7 @@ for i in `ls $Query_location/*.sparql`; do
         echo $k;
         #echo $k >> $RESULT_file;
         # execute
-        /usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $i> /dev/null 2>> /dev/null;
+        /usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $i> /dev/null 2>> /dev/null;
     done
 done
+
