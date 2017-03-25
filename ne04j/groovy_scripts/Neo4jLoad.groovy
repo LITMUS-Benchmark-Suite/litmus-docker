c788adce11aa8ff5a97446df40d34352fdca2180
yashwantkeswani <yashwant.keswani@gmail.com>
2017-03-26 01:39:19 +0530

    Update Neo4j Script

--- ne04j/groovy_scripts/Neo4jLoad.groovy
+++ ne04j/groovy_scripts/Neo4jLoad.groovy
@@ -1,9 +1,5 @@
-import com.tinkerpop.blueprints.impls.sparksee.*
-
 x = new Neo4jGraph(args[0])
-println "===============Loading the Graph Model============"
 s = System.currentTimeMillis()
 x.loadGraphML(args[1])
 println System.currentTimeMillis() - s
-println "===============Graph Model Loaded================="
 x.shutdown()
