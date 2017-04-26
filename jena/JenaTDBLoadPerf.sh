277c08e28cc1e3da43bc48c74703e7908c0590a1
yashwantkeswani <yashwant.keswani@gmail.com>
2017-04-26 14:58:16 +0530

    Redirectoutput to /dev/null

--- jena/JenaTDBLoadPerf.sh
+++ jena/JenaTDBLoadPerf.sh
@@ -2,17 +2,13 @@
 
 ##
 # Script to load a dataset in JenaTDB
-
-#Settings
 export Jena_Engine=/apache-jena-3.2.0/bin
 export PATH=$PATH:$Jena_Engine/bin:$Jena_Engine/lib
 
-# Input Parameters
-#setParamsGetQuery
 DB_location=$1; #path where the dataset is located
 DB_name=$2; #Datagraph 
 Dataset_name=$3; #Input Dataset 
 RESULT_file=$4;#Filename where the output of the loading process will be stored
 
 #load
-/usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" $Jena_Engine/tdbloader --verbose --loc=$DB_location/$DB_name $Dataset_name /dev/null 2>> /dev/null;
+/usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" /apache-jena-3.2.0/bin/tdbloader --verbose --loc=$DB_location/$DB_name $Dataset_name > /dev/null 2>> /dev/null;
