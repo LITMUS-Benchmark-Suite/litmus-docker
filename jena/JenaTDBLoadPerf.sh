3bdd86da31fbae03d73dc84cbb1f50ec889bb220
Yashwant Keswani <yashwant.keswani@gmail.com>
2017-04-29 16:30:58 +0200

    Update jena perf load script

--- jena/JenaTDBLoadPerf.sh
+++ jena/JenaTDBLoadPerf.sh
@@ -11,4 +11,7 @@ Dataset_name=$3; #Input Dataset
 RESULT_file=$4;#Filename where the output of the loading process will be stored
 
 #load
-/usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" /apache-jena-3.2.0/bin/tdbloader --verbose --loc=$DB_location/$DB_name $Dataset_name > /dev/null 2>> /dev/null;
+#/usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" /apache-jena-3.2.0/bin/tdbloader --verbose --loc=$DB_location/$DB_name $Dataset_name > /dev/null 2>> /dev/null;
+
+/usr/bin/time -a -o $RESULT_file -f "%S\t%U\t%e" /apache-jena-3.2.0/bin/tdbloader2 --phase data --loc=$DB_location/$DB_name $Dataset_name
+
