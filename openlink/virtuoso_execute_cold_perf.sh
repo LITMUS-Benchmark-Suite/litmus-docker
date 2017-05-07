4f1a133999011361fbc4c7440d73e3663bd34d5b
Harsh THAKKAR <thakkar@drogon.iai.uni-bonn.de>
2017-05-07 17:00:08 +0200

    Remove redundant argument

--- openlink/virtuoso_execute_cold_perf.sh
+++ openlink/virtuoso_execute_cold_perf.sh
@@ -1,6 +1,5 @@
 Virtuoso_isql=$1;
 RESULT_FILE=$2;
-Query_location=$3;
-SPARQL_FILE=$4;
+SPARQL_FILE=$3;
 /usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $SPARQL_FILE> /dev/null 2>> /dev/null;
 
---
+++ openlink/virtuoso_query_load.sh
@@ -0,0 +1,5 @@
+
+
+Virtuoso_isql=$1;
+
+$Virtuoso_isql 1111 dba dba /scripts/virtuoso/load.sql> /dev/null 2>> /dev/null;
