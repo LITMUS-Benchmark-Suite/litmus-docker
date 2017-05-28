6ad523fd54ca88f5ba66f963c667c3bff4ee59f9
yashwantkeswani <yashwant.keswani@gmail.com>
2017-05-28 18:18:49 +0530

    Update Virtuoso Script

--- openlink/virtuoso_execute_cold_perf.sh
+++ openlink/virtuoso_execute_cold_perf.sh
@@ -1,5 +1,5 @@
 Virtuoso_isql=$1;
 RESULT_FILE=$2;
 SPARQL_FILE=$3;
-/usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $SPARQL_FILE> /dev/null 2> virtuoso_query_cold;
+/usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $SPARQL_FILE >virtuoso_query_cold 2> /dev/null;
 
--- openlink/virtuoso_execute_hot_perf.sh
+++ openlink/virtuoso_execute_hot_perf.sh
@@ -2,5 +2,4 @@ Virtuoso_isql=$1;
 RESULT_FILE=$2;
 SPARQL_FILE=$3;
 
-/usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $SPARQL_FILE> /dev/null 2 > virtuoso_query_hot;
-
+/usr/bin/time -a -o $RESULT_FILE -f "%S\t%U\t%e" $Virtuoso_isql 1111 dba dba $SPARQL_FILE >virtuoso_query_hot 2> /dev/null;
